628862290f6723ffd09158cdc8cc0f9dde66cfd5
Mike Olson <mwolson@member.fsf.org>
2018-06-22 17:08:14 -0700

    Remove create-react-app boilerplate, hide outer scrollbars

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { doc } from 'storybook-readme';
-import AboutMd from '../lessons/00-about.md'
-import SetupMd from '../lessons/00-computer-setup.md'
-import Lesson1Md from '../lessons/01-modify-sample-project.md'
-import Lesson2Md from '../lessons/02-create-new-project.md'
-import CurrentHomeworkMd from '../lessons/98-current-homework.md'
-import PreviousHomeworkMd from '../lessons/99-previous-homework.md'
+import AboutMd from './00-about.md'
+import SetupMd from './00-computer-setup.md'
+import Lesson1Md from './01-modify-sample-project.md'
+import Lesson2Md from './02-create-new-project.md'
+import CurrentHomeworkMd from './98-current-homework.md'
+import PreviousHomeworkMd from './99-previous-homework.md'
 
 storiesOf('First Things', module)
   .add('About These Lessons', doc(AboutMd))
